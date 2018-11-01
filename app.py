4fbfa94a65852c5d5d8c67650ccb75da942f6d8d
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2018-11-01 14:43:50 +0100

    (refactor) removed unused print :broom:

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from scrape import main
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
-        Present the data in a RESTful API
+    Present the data in a RESTful API
     """
-    # return jsonify(predictions=main())
     return jsonify(main())
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 """
-    Program => Scrape data from SoccerWay
+    Program => Scrape data from StatArea
     Author => Samuel Afolaranmi
     Date => 01-04-2018
 """
@@ -9,15 +9,14 @@ import requests
 
 URL = 'https://www.statarea.com/predictions/date/{}/competition'
 
+
 def scrape():
     """
-        Scrape data from SoccerWay
+    Scrape data from StatArea
     """
     url = URL.format(time.strftime("%Y-%m-%d"))
-    print(url)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    # matches = soup.find_all('div', class_="cmatch")
 
     # Get all competitions
     all_competitions = []
@@ -25,14 +24,21 @@ def scrape():
 
     # Remove divs holding advertisements
     for item in competitions:
-        league_name = item.find('div', {'class':'header'}).find('div', {'class':'name'}).get_text()
+        league_name = item.find(
+            'div',
+            {'class': 'header'}
+        ).find(
+            'div',
+            {'class': 'name'}
+        ).get_text()
         if league_name != 'Advertisement':
             all_competitions.append(item)
     return all_competitions
 
+
 def get_league():
     """
-        Gets soccer data from the function scrape's raw data
+    Gets soccer data from the function scrape's raw data
     """
     all_scrape = scrape()
     elements = []
@@ -41,25 +47,54 @@ def get_league():
         league_predictions = []
 
         # Get the league from the header div
-        leagues['name'] = item.find('div', {'class':'header'}).find('div', {'class':'name'}).get_text()
+        leagues['name'] = item.find(
+            'div',
+            {'class': 'header'}
+        ).find(
+            'div',
+            {'class': 'name'}
+        ).get_text()
 
-        prediction = item.find('div', {'class':'body'})
-        matches = prediction.find_all("div", {"class":"match"})
+        prediction = item.find('div', {'class': 'body'})
+        matches = prediction.find_all("div", {"class": "match"})
 
         for match in matches:
             predictions = {}
-            matchrow = match.find("div", {"class":"matchrow"})
-            predictions['match_time'] = match.find("div", {"class":"date"}).get_text()
-            predictions['tip'] = matchrow.find("div",{"class":"tip"}).find("div", {"class":"value"}).find("div").get_text()
-            predictions['home_team'] = matchrow.find("div", {"class":"teams"}).find("div", {"class":"hostteam"}).find("div", {"class":"name"}).find("a").get_text()
-            predictions['away_team'] = matchrow.find("div", {"class":"teams"}).find("div", {"class":"guestteam"}).find("div", {"class":"name"}).find("a").get_text()
-            print(predictions['home_team'])
+            matchrow = match.find("div", {"class": "matchrow"})
+            predictions['match_time'] = match.find("div", {"class": "date"}).get_text()
+            predictions['tip'] = matchrow.find(
+                "div",
+                {"class": "tip"}
+            ).find(
+                "div",
+                {"class": "value"}
+            ).find("div").get_text()
+            predictions['home_team'] = matchrow.find(
+                "div",
+                {"class": "teams"}
+            ).find(
+                "div",
+                {"class": "hostteam"}
+            ).find(
+                "div",
+                {"class": "name"}
+            ).find("a").get_text()
+            predictions['away_team'] = matchrow.find(
+                "div",
+                {"class": "teams"}
+            ).find(
+                "div",
+                {"class": "guestteam"}
+            ).find(
+                "div",
+                {"class": "name"}
+            ).find("a").get_text()
             league_predictions.append(predictions)
 
             # Get statistics details             
             stats = []
-            statsrow = match.find("div", {"class":"inforow"}).find("div", {"class":"coefrow"})
-            all_stats = statsrow.find_all("div", {"class":"coefbox"})
+            statsrow = match.find("div", {"class": "inforow"}).find("div", {"class": "coefrow"})
+            all_stats = statsrow.find_all("div", {"class": "coefbox"})
             for stat in range(0, len(all_stats)):
                 stats.append(all_stats[stat].get_text())
             predictions['stats'] = stats[11:]
@@ -68,12 +103,13 @@ def get_league():
         elements.append(leagues)
     return elements
 
+
 def main():
     """
-        Prints out the scraped data
+    Prints out the scraped data
     """
-    # the_data = get_data()
     return get_league()
+
 
 if __name__ == '__main__':
     main()
